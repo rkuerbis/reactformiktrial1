4709c7b3ac8428ec98504b4300c7ba0ecc4d22d2
rkuerbis <rkuerbis@hotmail.com>
2019-05-10 03:13:17 -0700

    Recommit Initial.
    Saved github files improperly.

--- src/index.js
+++ src/index.js
@@ -1,16 +1,32 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { withFormik } from "formik";
+import Yup from "yup";
 
 import "./styles.css";
 
-function App() {
+const App = ({ values }) => {
   return (
     <div className="App">
       <h1>Hello CodeSandbox</h1>
       <h2>Start editing to see some magic happen!</h2>
+      <input
+        type="email"
+        name="email"
+        placeholder="Email"
+        value={values.email}
+      />
     </div>
   );
-}
+};
+
+const FormikApp = withFormik({
+  mapPropsToValues() {
+    return {
+      email: "test text"
+    };
+  }
+})(App);
 
 const rootElement = document.getElementById("root");
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(<FormikApp />, rootElement);
