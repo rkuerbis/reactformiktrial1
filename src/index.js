d95b6deefcac3e9c347434357b631a77de1a9a32
rkuerbis <rkuerbis@hotmail.com>
2019-05-10 03:58:03 -0700

    With html form statement.

--- src/index.js
+++ src/index.js
@@ -1,33 +1,52 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { withFormik } from "formik";
+import { withFormik, Form } from "formik";
 import Yup from "yup";
 
 import "./styles.css";
 
-const App = ({ values, handleChange }) => {
+const App = ({ values, handleChange, handleSubmit }) => {
   return (
     <div className="App">
       <h1>Hello CodeSandbox</h1>
       <h2>Start editing to see some magic happen!</h2>
-      <input
-        type="email"
-        name="email"
-        placeholder="Email"
-        value={values.email}
-        onChange={handleChange}
-      />
+
+      <form onSubmit={handleSubmit}>
+        <input
+          type="email"
+          name="email"
+          placeholder="Email"
+          value={values.email}
+          onChange={handleChange}
+        />
+
+        <input
+          type="password"
+          name="password"
+          placeholder="Password"
+          value={values.password}
+          onChange={handleChange}
+        />
+        <button type="submit">Submit</button>
+      </form>
     </div>
   );
 };
 
 const FormikApp = withFormik({
-  mapPropsToValues({ email }) {
+  mapPropsToValues({ email, password }) {
     return {
-      email: email || ""
+      email: email || "",
+      password: password || ""
     };
+  },
+  handleSubmit(values) {
+    console.log(values);
   }
 })(App);
 
 const rootElement = document.getElementById("root");
-ReactDOM.render(<FormikApp email="rkuerbis@gmail.com" />, rootElement);
+ReactDOM.render(
+  <FormikApp email="rkuerbis@gmail.com" password="nonepassword" />,
+  rootElement
+);
